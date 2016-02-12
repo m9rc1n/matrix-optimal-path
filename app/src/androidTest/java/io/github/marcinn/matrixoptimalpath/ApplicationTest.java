827c923c70fcf3301a18ef27e3071246e3102eee
marc-inn <0.660162@gmail.com>
2016-02-12 21:07:56 +0100

    Refactor espresso test

--- app/src/androidTest/java/io/github/marcinn/matrixoptimalpath/ApplicationTest.java
+++ app/src/androidTest/java/io/github/marcinn/matrixoptimalpath/ApplicationTest.java
@@ -47,6 +47,25 @@ public class ApplicationTest {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
             MainActivity.class);
 
+    public static ViewAction setProgress(final int progress) {
+        return new ViewAction() {
+            @Override
+            public void perform(UiController uiController, View view) {
+                ((DiscreteSeekBar) view).setProgress(progress);
+            }
+
+            @Override
+            public String getDescription() {
+                return "Set a progress";
+            }
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return ViewMatchers.isAssignableFrom(DiscreteSeekBar.class);
+            }
+        };
+    }
+
     @Test
     public void testIfPreferenceFragmentWillShowUp() throws Exception {
         checkPreferenceFragmentMatching(matches(not(isDisplayed())));
@@ -80,31 +99,50 @@ public class ApplicationTest {
         WordCell c2 = new WordCell("c", 2);
         WordCell c3 = new WordCell("d", 3);
         WordCell c4 = new WordCell("e", 4);
-        onView((withId(R.id.recyclerView_table))).perform(scrollToHolder(new TableViewHolderMatcher()).atPosition(0));
+        onView((withId(R.id.recyclerView_table))).perform(scrollToHolder(new TableViewHolderMatcher())
+                .atPosition(0));
         onView(withText(c0.toString())).check(matches(isDisplayed()));
-        onView((withId(R.id.recyclerView_table))).perform(scrollToHolder(new TableViewHolderMatcher()).atPosition(1));
+        onView((withId(R.id.recyclerView_table))).perform(scrollToHolder(new TableViewHolderMatcher())
+                .atPosition(1));
         onView(withText(c1.toString())).check(matches(isDisplayed()));
-        onView((withId(R.id.recyclerView_table))).perform(scrollToHolder(new TableViewHolderMatcher()).atPosition(2));
+        onView((withId(R.id.recyclerView_table))).perform(scrollToHolder(new TableViewHolderMatcher())
+                .atPosition(2));
         onView(withText(c2.toString())).check(matches(isDisplayed()));
-        onView((withId(R.id.recyclerView_table))).perform(scrollToHolder(new TableViewHolderMatcher()).atPosition(3));
+        onView((withId(R.id.recyclerView_table))).perform(scrollToHolder(new TableViewHolderMatcher())
+                .atPosition(3));
         onView(withText(c3.toString())).check(matches(isDisplayed()));
-        onView((withId(R.id.recyclerView_table))).perform(scrollToHolder(new TableViewHolderMatcher()).atPosition(4));
+        onView((withId(R.id.recyclerView_table))).perform(scrollToHolder(new TableViewHolderMatcher())
+                .atPosition(4));
         onView(withText(c4.toString())).check(matches(isDisplayed()));
     }
 
     @Test
     public void testWithAddTextButton() throws Exception {
         onView(withId(R.id.action_preference)).perform(click());
-        onView(withId(R.id.seekBar_columnsNumber)).perform(setProgress(10));
         onView(withId(R.id.imageButton_appendText)).perform(click());
-        WordCell c0 = new WordCell("pieniądze", 528);
+        onView(withId(R.id.seekBar_columnsNumber)).perform(setProgress(10));
+
+        WordCell c0 = new WordCell("non", 232);
         onView(withItemText(c0.toString())).check(doesNotExist());
-        onView((withId(R.id.recyclerView_table))).perform(scrollToPosition(2608));
+        onView((withId(R.id.recyclerView_table))).perform(scrollToPosition(999));
         onView(withItemText(c0.toString())).check(matches(isDisplayed()));
+
+        onView(withId(R.id.action_preference)).perform(click());
+        onView(withId(R.id.action_preference)).perform(click());
+
+        onView(withId(R.id.imageButton_appendText)).perform(click());
+        onView(withId(R.id.seekBar_columnsNumber)).perform(setProgress(20));
+
+        WordCell c1 = new WordCell("non", 204);
+        onView(withItemText(c1.toString())).check(doesNotExist());
+        onView((withId(R.id.recyclerView_table))).perform(scrollToPosition(1999));
+        onView(withItemText(c1.toString())).check(matches(isDisplayed()));
+
     }
 
     private void checkIfKeyboardIsClosed() {
-        InputMethodManager imm = (InputMethodManager) mActivityRule.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        InputMethodManager imm = (InputMethodManager) mActivityRule.getActivity()
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
         Assert.assertTrue("Keyboard should be closed in Table Fragment", !imm.isAcceptingText());
     }
 
@@ -153,24 +191,5 @@ public class ApplicationTest {
         public void describeTo(Description description) {
             description.appendText("is assignable from CustomViewHolder");
         }
-    }
-
-    public static ViewAction setProgress(final int progress) {
-        return new ViewAction() {
-            @Override
-            public void perform(UiController uiController, View view) {
-                ((DiscreteSeekBar) view).setProgress(progress);
-            }
-
-            @Override
-            public String getDescription() {
-                return "Set a progress";
-            }
-
-            @Override
-            public Matcher<View> getConstraints() {
-                return ViewMatchers.isAssignableFrom(DiscreteSeekBar.class);
-            }
-        };
     }
 }
